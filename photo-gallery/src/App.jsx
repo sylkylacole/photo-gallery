5af2dd85d526e8e6a746e7198c06d4d4820fdcd8
Syl'Kyla Cole <sylkylacole@gmail.com>
2025-03-03 16:46:37 -0600

    RANDOM ERROR; SEE YOU AFTER AN 8 HOUR SLEEP

--- photo-gallery/src/App.jsx
+++ photo-gallery/src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-    <div className="container text-[#e0e1dd] overflow-hidden">
-      <aside className="w-[100%] h-[100vh] bg-[#415A77] text-platinum p-4 bg-ox rounded-r-4xl">
+      <div className="container text-[#e0e1dd]">
+        <aside className="w-[100%] h-[100vh] bg-[#415A77] text-platinum p-4 bg-ox rounded-r-4xl">
           <img src={kinich} alt="logo" className="logo mb-[2rem] rounded-4xl"></img>
           <p className="aside-description text-justify text-xl">Welcome to my personal highlights! To learn more about the picture, click the photo! To get a brief note about the photo, hover over it for a bit. If anything is broken or you want to reach me, feel free to visit my socials down below or join the Discord!</p>
           <div id="links" className="text-xl">
@@ -15,35 +15,34 @@ function App() {
             <a target="_blank" href="https://www.github.com/sylkylacole"><p className="text-[#e0e1dd] hover:text-[#0D1B2A]">Github</p></a>
             <a target="_blank" href="mailto:sylkylacole@gmail.com"><p className="text-[#e0e1dd] hover:text-[#0D1B2A]">Email</p></a>
           </div>
-      </aside>
-      <div className="img-container bg-[#1B263B]">
-        <div className="row m-[2rem]" id="row1">
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-        </div>
-        <div className="row m-[2rem]" id="row1">
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-        </div>
-        <div className="row m-[2rem]" id="row1">
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-        </div>
-        <div className="row m-[2rem]" id="row1">
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
-          <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+        </aside>
+        <div className="img-container bg-[#1B263B]">
+          <div className="row m-[2rem]" id="row1">
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl coolborder"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+          </div>
+          <div className="row m-[2rem]" id="row2">
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+          </div>
+          <div className="row m-[2rem]" id="row3">
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+          </div>
+          <div className="row m-[2rem]" id="row4">
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+            <img src={kinich} alt="placeholder-image" className="gallery-photo h-[25vh] w-[20vw] rounded-3xl"></img>
+          </div>
         </div>
       </div>
-    </div>
-      
     </>
   )
 }
